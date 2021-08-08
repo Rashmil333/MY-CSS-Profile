d6f3c14e0e727dc9a0cef57594d7f76115382b29
Rashmil333 <68959771+Rashmil333@users.noreply.github.com>
2021-08-09 04:33:45 +0530

    commit

---
+++ Cssprofile.jsx
@@ -0,0 +1,305 @@
+import React from "react";
+import SearchIcon from '@material-ui/icons/Search';
+const Cssprofile=()=>{
+	return(<>
+		<a href="/">back</a>
+
+		<div class="g_pulse">
+			<div class="g_pulse_center">
+				<a   class="a_pulse">Hello from the pulse side.</a>
+			</div>
+			<div  class="butterfly">
+				<img  src="https://i.pinimg.com/originals/d7/0b/9b/d70b9bcf575f8ca592c19a7685b86920.gif" style={{width:'150px',height:'150px'}}/>
+				<p class="butterfly_p"></p>
+			</div>
+
+			
+		</div>
+
+		<div class="g_envelope">
+				<p> Hello from the envelope side.</p>
+		
+		</div>
+
+		<p class="g_selection">Hello from the selection side.</p>
+
+
+		<div class="g_parent_div">
+			<div class="g_child_div">
+			</div>
+		</div>
+
+		<div class="g_test1">
+			<p class="g_p2">Hello from the before and after side.</p>
+
+		</div>
+
+		<div class='g_div2'>
+			<p class="g_p3">Hello from the another before and after side.</p>
+			
+		</div>
+
+		<div class="g_first_letteerdiv" >
+			<p class='g_parafirst'>
+				Hello from the first letter side. And therefore we can easily say that it is
+				is very important to notice and therefore we have to try it very easily so that.
+				Hello from the first letter side. And therefore we can easily say that it is
+				is very important to notice and therefore we have to try it very easily so that.
+				Hello from the first letter side. And therefore we can easily say that it is
+				is very important to notice and therefore we have to try it very easily so that.
+				Hello from the first letter side. And therefore we can easily say that it is
+				is very important to notice and therefore we have to try it very easily so that.
+				Hello from the first letter side. And therefore we can easily say that it is
+				is very important to notice and therefore we have to try it very easily so that.
+		
+				
+			</p>
+			
+		</div>
+
+		<div class="g_blend_div">
+			<p class="g_parablend">Hello from the blend mode side.</p>
+		
+		</div>
+
+		<div class='g_lidiv'>
+			<ol >
+				<li class="g_liforblend">One </li>
+				<li class="g_liforblend" >Two</li>
+				<li class="g_liforblend">Three</li>
+				<li class="g_liforblend">Four</li>
+				<li class="g_liforblend">Five</li>
+				<li class="g_liforblend">Six</li>
+				<li class="g_liforblend">Seven</li>
+				<li class="g_liforblend">Eight</li>
+				<li class="g_liforblend">Nine</li>
+				<li class="g_liforblend">Tenth </li>
+				
+			</ol>
+			
+
+		</div>
+
+		<div class="g_backgroundtext">
+			<p class="g_new_text">Hello from the backgorund text side</p>
+			
+		</div>
+		<div class="g_credit_card">
+			<div class="g_main_card">
+					<div class='g_top_div'>
+				<p>CREDIT</p>
+				<p>VISA</p>
+				
+			</div>
+			<div class="g_bottom_div">
+				<p>Rashmil Rajoot</p>
+				<p>100-789-654-789</p>
+			</div>
+			</div>
+		
+			
+		</div>
+
+		<div class="g_loader_div">
+			<div class="g_loader_center_div">
+				<div class="g_loader"></div>
+			</div>
+		</div>
+
+		<div class="g_heartdiv">
+			<div class="g_heart">
+				
+			</div>
+			
+		</div>
+		<div class="g_slicing_div">
+			<p class="g_slicing">Hello from the slicing side</p>
+			
+		</div>
+
+		<div class="g_thunder_div">
+			<div class="g_thunder_main">
+				<ul>
+					<li>t</li>
+					<li>h</li>
+					<li>u</li>				
+					<li>n</li>
+					<li>d</li>
+					<li>e</li>
+					<li>r</li>
+				</ul>		
+			</div>
+		</div>
+
+		<div class="g_pendulum_div">
+			<div class="g_pendulum_main">
+				<span class="g_pendulum"></span>
+				<span  class="g_pendulum"></span>
+				<span  class="g_pendulum"></span>
+				<span  class="g_pendulum"></span>
+				<span  class="g_pendulum"></span>
+				<span  class="g_pendulum"></span>
+				<span  class="g_pendulum"></span>	
+			</div>
+		</div>
+		<div class="g_box_div">
+			<div class="g_box_center_div">
+				<span></span>
+			</div>
+		</div>
+
+		<div class="g_card_div">
+			<div class="g_card_center_div">
+				<img src="https://images.pexels.com/photos/1172064/pexels-photo-1172064.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" style={{width:'600px',marginLeft:'-50px',marginTop:'-10px'}}/>
+			</div>
+		</div>
+
+		<div class="g_sliding_button_div">
+			<div class="g_sliding_center_div">
+				<p>Hello from the sliding button side.</p>
+				
+
+			</div>
+			
+		</div>
+
+		<div class="g_curtain_div">
+			<div class="g_curtain_center_div">
+				<p>Hello from the Curtain Effect side.</p>
+				
+			</div>
+			
+		</div>
+
+		<div class="g_music_beat">
+			<div class="g_music_center_div">
+				<ul>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="g_preloader">
+			<div class="g_preloader_center_div">
+			</div>
+		</div>
+
+		<div class="g_gitdiv">
+			<div class="g_git_main">
+				<div class="g_git1"></div>
+				<div class="g_git2"></div>
+				<div class="g_git3"></div>
+			</div>
+		</div>
+		<div class="g_instagram">
+			<div class="g_instagram_main">
+				<div class="g_i_border1">
+					<div class="g_i_border2">
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="g_react">
+			<div class="g_react_main">
+				<div class='g_circle'>
+				</div>
+			</div>
+		</div>
+
+		<div class="g_camera">
+			<div class="g_camera_center_div">
+				<div class="g_camera_btn_div">
+					<div class="g_camera1">
+						<div class="g_camera2">
+							<div class="g_camera3">	
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="g_panda">
+			<div class="g_panda_main">
+				<div class="g_eye">
+					<div class="g_pupil">
+					</div>
+				</div>
+				<div class="g_eyeright">
+					<div class="g_pupil2">
+					</div>
+				</div>
+				<div class="g_face">
+				</div>
+			</div>
+		</div>
+
+		<div class="g_hidden_search">
+			<div class="g_hidden_search_center">
+				<input class="g_input" type="text" placeholder="type text"/>
+				<button class="g_btn"><SearchIcon style={{width:'40px',height:'50px'}}/></button>
+				
+			</div>
+			
+		</div>
+
+
+		<div class="g_test1">
+			<div class="g_test1_center">
+			</div>
+		</div>
+
+		<div class="g_test2">
+			<div class="g_test2_center">
+			</div>
+		</div>
+
+		<div class="g_test3">
+			<div class="g_test3_center">
+				<div class="g_test3_center_in">
+				</div>
+			</div>
+		</div>
+
+		<div class="bounce">
+			<div class="bounce_center">
+				<div class="bounce_ball"></div>
+				<div class="bar"></div>
+			</div>
+		</div>
+
+		<div class="watch">
+			<div class="watch_center">
+				<div class="second"></div>
+				
+			</div>
+			
+
+		</div>	
+
+		<div class="pulso_main">
+			<a href="#" class='pulso'>Hello</a>
+
+
+		</div>
+		
+
+
+
+
+
+	</>)
+}
+
+export default Cssprofile;
